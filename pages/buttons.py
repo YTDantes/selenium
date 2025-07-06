ddca070bbeb64473b2fd9b99bb0b25bdfc8130a2
s_dantes <dan12332123@gmail.com>
2025-07-06 21:13:04 +0300

    change strategy

--- pages/buttons.py
+++ pages/buttons.py
@@ -5,12 +5,17 @@ class ButtonsCheck:
 
     def open(self):
         self.driver.get("https://demoqa.com/selectable")
+        self.driver.save_screenshot("headless.png")
 
     def select_grid(self):
         self.driver.find_element("xpath", "//a[@data-rb-event-key='grid']").click()
+        self.driver.save_screenshot("headless1.png")
+
 
     def check_buttons(self, BUTTONS):
         buttons = self.driver.find_elements(*BUTTONS)
+        self.driver.save_screenshot("headless3.png")
+
         for button in buttons:
             button.click()
             status = button.get_attribute("class")
